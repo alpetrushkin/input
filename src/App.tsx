90d4f10c7b52608d8c4e99712fb015e239a56f6a
alpetrushkin <alpetrushkin1984@mail.ru>
2022-08-03 08:29:55 +0300

    fullinput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {FullInput} from "./components/FullInput";
 
 function App() {
    let [message, setMessage] = useState([
@@ -9,12 +9,14 @@ function App() {
       {message: 'message3'},
    ])
 
+   const addMessage = (title: string) => {
+      let newMessage = {message: title}
+      setMessage([newMessage, ...message])
+   }
+
    return (
       <div className="App">
-         <div>
-            <input/>
-            <button>+</button>
-         </div>
+         <FullInput addMessage={addMessage}/>
          {message.map((el, index) => {
             return (
                <div key={index}>{el.message}</div>
